a62272fa26575afe684a9740ad1b701e96be2270
Aiden H <aiden.hernandez@d11.org>
2024-09-17 19:09:29 +0000

    Did Assignment.

--- intro.py
+++ intro.py
@@ -1,37 +1,40 @@
 # 1. Print "Hello World!" to the console
-
+print("Hello World!")
 
 
 # 2. Print "Hello my name is ______!" to the console
-
+print("Hello my name is Aiden!")
 
 
 # 3. Print "I love python!" to the console
 
-
+print("I love python!")
 
 # 4. Print your age to the console
 
-
+print("My age is 14")
 
 # 5. Print 5+5
-
+print(5+5)
 
 
 # 6. Print 4*3
-
+print(4*3)
 
 
 # 7. Print 50 - 40
 
-
+print(50-40)
 
 # 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
 
-
+print(100/5)
+print (100//5)
+#The difference here is that line 31 has a slash and doesnt put the numbers in a float.
 
 # 9. Print 2**4
-
+print(2**4)
 
 
 # 10. Print 6*3-4+8//2
+print(6*3-4+8//2)
